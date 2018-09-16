6162cd214314ee314dc9634cba262d6a387684d4
xieyucheng <648547525@qq.com>
2018-09-16 12:03:47 +0800

    delelte a.txt

---
+++ code/01/01/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  01
+//
+//  Created by 玉成 on 2018/9/16.
+//  Copyright © 2018年 weistek. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
